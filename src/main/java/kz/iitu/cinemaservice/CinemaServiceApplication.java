65565de6cf9d78311a52ffc1295e94269127c1fe
viowla <55015538+viowla@users.noreply.github.com>
2020-12-14 15:19:02 +0600

    reformat movie controller

--- src/main/java/kz/iitu/cinemaservice/CinemaServiceApplication.java
+++ src/main/java/kz/iitu/cinemaservice/CinemaServiceApplication.java
@@ -1,7 +1,9 @@
 package kz.iitu.cinemaservice;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -31,4 +33,13 @@ public class CinemaServiceApplication {
         SpringApplication.run(CinemaServiceApplication.class, args);
     }
 
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
